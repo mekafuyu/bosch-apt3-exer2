060b77887320e91b4d7cb829f64e337752c3fbde
Andre Dias <andredias.curitiba@gmail.com>
2024-04-03 22:29:16 -0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Card } from './components/Card'
 import produtos from './constants/produtos.json'
+import { api } from "./api/rmApi"
 import style from './App.module.css'
 
 function App() {
   const [show, setShow] = useState("")
+  const [data, setData] = useState([])
+  const [page, setPage] = useState("")
+
+
+  useEffect(() => {
+    api.get(`/character/?page=${page}`).then((response) => {
+      if(!response.data.results){
+        console.log("Vazio")
+      }
+      setData(response.data.results)
+    }).catch((error) => {
+      if(error.response.status === 404){
+        console.log("Esta pagina nao contem este personagem")
+      }
+      console.error(error)
+    })
+  }, [page])
 
   return (
     <>
@@ -31,7 +49,17 @@ function App() {
         <>
           <h2>Rick and Morty API</h2>
             <div>
-              
+            <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)}/>
+            </div>
+            <div>
+            {data.map((item) => { 
+          return(
+            <div key={item.id}>
+              <Card name={item.name} desc={item.species} value={item.gender} image={item.image} />
+              {/* <button onClick={() => {}}>Info</button> */}
+            </div>
+          )
+     })}
             </div>
        </>
       }
